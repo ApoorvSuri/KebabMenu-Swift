6139534b0b952fbed60b88293157585a6e578434
Apoorv Suri <apoorvsuri2012@gmail.com>
2019-07-26 21:16:36 +0530

    Initial commit

---
+++ KebabMenuView/KebabMenuView.h
@@ -0,0 +1,19 @@
+//
+//  KebabMenuView.h
+//  KebabMenuView
+//
+//  Created by Apoorv Suri on 26/07/19.
+//  Copyright © 2019 Apoorv Suri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KebabMenuView.
+FOUNDATION_EXPORT double KebabMenuViewVersionNumber;
+
+//! Project version string for KebabMenuView.
+FOUNDATION_EXPORT const unsigned char KebabMenuViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KebabMenuView/PublicHeader.h>
+
+
